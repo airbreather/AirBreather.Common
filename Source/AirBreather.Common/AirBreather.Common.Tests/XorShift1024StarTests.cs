dc3c74cfd1f733c55e5dbd3090e257ebb8eb2302
Joe Amenta <airbreather@linux.com>
2015-08-15 17:02:47 -0400

    Adding xorshift1024* Minor cleanup. Also lowering the size of these byte arrays in tests, because my poor PC can't handle it.

--- Source/AirBreather.Common/AirBreather.Common.Tests/XorShift1024StarTests.cs
+++ Source/AirBreather.Common/AirBreather.Common.Tests/XorShift1024StarTests.cs
@@ -8,38 +8,10 @@ using AirBreather.Common.Random;
 
 namespace AirBreather.Common.Tests
 {
-    public sealed class XorShift128PlusTests
+    public sealed class XorShift1024StarTests
     {
-        [Theory]
-        [InlineData(1234524356ul, 47845723665ul, 10356027574996968ul, 421627830503766283ul, 7267806761253193977ul)]
-        [InlineData(262151541652562ul, 468594272265ul, 3923822141990852456ul, 3993942717521754294ul, 13070632098572223408ul)]
-        public void Test(ulong s0, ulong s1, ulong expectedResult1, ulong expectedResult2, ulong expectedResult3)
-        {
-            // this was params ulong[], but I like InlineData too much to make that work...
-            ulong[] expectedResults = { expectedResult1, expectedResult2, expectedResult3 };
-
-            var gen = new XorShift128PlusGenerator();
-            var state = new XorShift128PlusState(s0, s1);
-            byte[] buf = new byte[expectedResults.Length * 8];
-
-            // First, do it in separate calls.
-            for (int i = 0; i < expectedResults.Length; i++)
-            {
-                state = gen.FillBuffer(state, buf, i * 8, 8);
-                Assert.Equal(expectedResults[i], BitConverter.ToUInt64(buf, i * 8));
-            }
-
-            // Now, do it all in one call.
-            state = new XorShift128PlusState(s0, s1);
-            state = gen.FillBuffer(state, buf, 0, buf.Length);
-            for (int i = 0; i < expectedResults.Length; i++)
-            {
-                Assert.Equal(expectedResults[i], BitConverter.ToUInt64(buf, i * 8));
-            }
-
-            // Now, ensure that it throws if we're out of alignment.
-            Assert.Throws<ArgumentException>("index", () => state = gen.FillBuffer(state, buf, 3, 8));
-        }
+        // I, too, like to live dangerously.
+        private const int pOffset = sizeof(ulong) * 16;
 
         [Theory]
         [InlineData(1234524356ul, 47845723665ul, 1)]
@@ -52,29 +24,31 @@ namespace AirBreather.Common.Tests
         public unsafe void SpeedTestSingleArray(ulong s0, ulong s1, int chunks)
         {
             // see how quickly we can fill the biggest possible buffer that doesn't require 2GB array sizes.
-            var gen = new XorShift128PlusGenerator();
+            var gen = new XorShift1024StarGenerator();
 
             // stage 1: set up the initial state, output buffer, and chunk size.
-            var initialState = new XorShift128PlusState(s0, s1);
+            XorShift1024StarState initialState = CreateInitialState(s0, s1);
 
             const int OutputBufferLength = 1 << 30;
             var outputBuffer = new byte[OutputBufferLength];
             var chunkSize = OutputBufferLength / chunks;
 
             // stage 2: use that state to set up the parallel independent states.
-            var parallelStateBuffer = new byte[sizeof(XorShift128PlusState) * chunks];
+            var parallelStateBuffer = new byte[sizeof(XorShift1024StarState) * chunks];
             gen.FillBuffer(initialState, parallelStateBuffer);
 
-            var parallelStates = new XorShift128PlusState[chunks];
+            var parallelStates = new XorShift1024StarState[chunks];
             fixed (byte* sFixed = parallelStateBuffer)
-            fixed (XorShift128PlusState* tFixed = parallelStates)
+            fixed (XorShift1024StarState* tFixed = parallelStates)
             {
-                var s = (XorShift128PlusState*)sFixed;
+                var s = (XorShift1024StarState*)sFixed;
                 var sEnd = s + chunks;
                 var t = tFixed;
 
                 while (s < sEnd)
                 {
+                    // zero out "p", as it can only be 0-15.
+                    *((int*)(((byte*)s) + pOffset)) = 0;
                     *(t++) = *(s++);
                 }
             }
@@ -94,7 +68,7 @@ namespace AirBreather.Common.Tests
             sw.Stop();
 
             double seconds = sw.ElapsedTicks / (double)Stopwatch.Frequency / (double)Reps;
-            Console.WriteLine("XorShift128PlusTests.SpeedTestSingleArray: Took an average of {0:N5} seconds to fill a single buffer with a size of {1:N0} bytes ({2:N5} GiB per second) by directly writing to {3} chunk(s).",
+            Console.WriteLine("XorShift1024StarTests.SpeedTestSingleArray: {0:N5} seconds, size of {1:N0} bytes ({2:N5} GiB per second), {3} separate chunk(s).",
                               seconds,
                               OutputBufferLength,
                               OutputBufferLength / seconds / (1 << 30),
@@ -116,29 +90,31 @@ namespace AirBreather.Common.Tests
         public unsafe void SpeedTestSeparateArraysWithMergeAtEnd(ulong s0, ulong s1, int chunks)
         {
             // see how quickly we can fill the biggest possible buffer that doesn't require 2GB array sizes.
-            var gen = new XorShift128PlusGenerator();
+            var gen = new XorShift1024StarGenerator();
 
             // stage 1: set up the initial state, output buffer, and chunk size.
-            var initialState = new XorShift128PlusState(s0, s1);
+            XorShift1024StarState initialState = CreateInitialState(s0, s1);
 
             const int OutputBufferLength = 1 << 30;
             var outputBuffer = new byte[OutputBufferLength];
             var chunkSize = OutputBufferLength / chunks;
 
             // stage 2: use that state to set up the parallel independent states.
-            var parallelStateBuffer = new byte[sizeof(XorShift128PlusState) * chunks];
+            var parallelStateBuffer = new byte[sizeof(XorShift1024StarState) * chunks];
             gen.FillBuffer(initialState, parallelStateBuffer);
 
-            var parallelStates = new XorShift128PlusState[chunks];
+            var parallelStates = new XorShift1024StarState[chunks];
             fixed (byte* sFixed = parallelStateBuffer)
-            fixed (XorShift128PlusState* tFixed = parallelStates)
+            fixed (XorShift1024StarState* tFixed = parallelStates)
             {
-                var s = (XorShift128PlusState*)sFixed;
+                var s = (XorShift1024StarState*)sFixed;
                 var sEnd = s + chunks;
                 var t = tFixed;
 
                 while (s < sEnd)
                 {
+                    // zero out "p", as it can only be 0-15.
+                    *((int*)(((byte*)s) + pOffset)) = 0;
                     *(t++) = *(s++);
                 }
             }
@@ -180,7 +156,7 @@ namespace AirBreather.Common.Tests
             // HOWEVER, the other one has one particular disadvantage: it fixes
             // the *entire* array of just under 2 GiB for the duration.
             double seconds = sw.ElapsedTicks / (double)Stopwatch.Frequency / (double)Reps;
-            Console.WriteLine("XorShift128PlusTests.SpeedTestSeparateArraysWithMergeAtEnd: Took an average of {0:N5} seconds to fill a single buffer with a size of {1:N0} bytes ({2:N5} GiB per second) by writing to {3} new chunk(s) in parallel and merging.",
+            Console.WriteLine("XorShift1024StarTests.SpeedTestSeparateArraysWithMergeAtEnd: {0:N5} seconds, size of {1:N0} bytes ({2:N5} GiB per second), {3} separate chunk(s).",
                               seconds,
                               OutputBufferLength,
                               OutputBufferLength / seconds / (1 << 30),
@@ -189,6 +165,23 @@ namespace AirBreather.Common.Tests
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
+        }
+
+        private static unsafe XorShift1024StarState CreateInitialState(ulong s0, ulong s1)
+        {
+            // Use xorshift128+ to generate the state for xorshift1024*.
+            var gen = new XorShift128PlusGenerator();
+            var state = new XorShift128PlusState(s0, s1);
+
+            byte[] buf = new byte[sizeof(ulong) * 16 + Math.Max(sizeof(int), XorShift128PlusGenerator.ChunkSize)];
+            gen.FillBuffer(state, buf);
+
+            fixed (byte* fBuf = buf)
+            {
+                // zero out "p", as it can only be 0-15.
+                *((int*)(fBuf + pOffset)) = 0;
+                return *((XorShift1024StarState*)fBuf);
+            }
         }
     }
 }
