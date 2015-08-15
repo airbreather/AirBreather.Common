7af4b0b1c79df24b050e09ac3fad9e2d58076646
Joe Amenta <airbreather@linux.com>
2015-08-15 12:24:08 -0400

    Adding equatable struct types for sets. HashSetValue<T>: Backed by System.Collections.Immutable.ImmutableHashSet<T>. SortedSetValue<T>: Backed by System.Collections.Immutable.ImmutableSortedSet<T> (and thus GetHashCode() can be slow).

---
+++ Source/AirBreather.Common/AirBreather.Common/Collections/HashSetValue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace AirBreather.Common.Collections
+{
+    public struct HashSetValue<T> : IReadOnlySet<T>, IEquatable<HashSetValue<T>>
+    {
+        private ImmutableHashSet<T> values;
+
+        public HashSetValue(IEnumerable<T> values)
+        {
+            this.values = values.ToImmutableHashSet();
+        }
+
+        public HashSetValue(IEnumerable<T> values, IEqualityComparer<T> comparer)
+        {
+            this.values = values.ToImmutableHashSet(comparer);
+        }
+
+        public ImmutableHashSet<T> UnderlyingSet => this.values ?? ImmutableHashSet<T>.Empty;
+        public IEqualityComparer<T> KeyComparer => this.UnderlyingSet.KeyComparer;
+        public int Count => this.UnderlyingSet.Count;
+
+        public bool Contains(T item) => this.UnderlyingSet.Contains(item);
+        public bool IsProperSubsetOf(IEnumerable<T> other) => this.UnderlyingSet.IsProperSubsetOf(other);
+        public bool IsProperSupersetOf(IEnumerable<T> other) => this.UnderlyingSet.IsProperSupersetOf(other);
+        public bool IsSubsetOf(IEnumerable<T> other) => this.UnderlyingSet.IsSubsetOf(other);
+        public bool IsSupersetOf(IEnumerable<T> other) => this.UnderlyingSet.IsSupersetOf(other);
+        public bool Overlaps(IEnumerable<T> other) => this.UnderlyingSet.Overlaps(other);
+        public bool SetEquals(IEnumerable<T> other) => this.UnderlyingSet.SetEquals(other);
+        public ImmutableHashSet<T>.Enumerator GetEnumerator() => this.UnderlyingSet.GetEnumerator();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public static bool Equals(HashSetValue<T> first, HashSetValue<T> second) => first.SetEquals(second);
+        public static bool operator ==(HashSetValue<T> first, HashSetValue<T> second) => Equals(first, second);
+        public static bool operator !=(HashSetValue<T> first, HashSetValue<T> second) => !Equals(first, second);
+        public override bool Equals(object obj) => obj is HashSetValue<T> && Equals(this, (HashSetValue<T>)obj);
+        public bool Equals(HashSetValue<T> other) => Equals(this, other);
+        public override int GetHashCode() => GetHashCode(this);
+        public static int GetHashCode(HashSetValue<T> value)
+        {
+            ImmutableHashSet<T> underlyingSet = value.UnderlyingSet;
+
+            int hashCode = underlyingSet.Count;
+            foreach (T element in underlyingSet)
+            {
+                hashCode ^= underlyingSet.KeyComparer.GetHashCode(element);
+            }
+
+            return hashCode;
+        }
+    }
+}
---
+++ Source/AirBreather.Common/AirBreather.Common/Collections/SortedSetValue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AirBreather.Common.Collections
+{
+    public struct SortedSetValue<T> : IReadOnlySet<T>, IEquatable<SortedSetValue<T>>
+    {
+        private ImmutableSortedSet<T> values;
+
+        public SortedSetValue(IEnumerable<T> values)
+        {
+            this.values = values.ToImmutableSortedSet();
+        }
+
+        public SortedSetValue(IEnumerable<T> values, IComparer<T> comparer)
+        {
+            this.values = values.ToImmutableSortedSet(comparer);
+        }
+
+        public ImmutableSortedSet<T> UnderlyingSet => this.values ?? ImmutableSortedSet<T>.Empty;
+        public IComparer<T> KeyComparer => this.UnderlyingSet.KeyComparer;
+        public int Count => this.UnderlyingSet.Count;
+
+        public bool Contains(T item) => this.UnderlyingSet.Contains(item);
+        public bool IsProperSubsetOf(IEnumerable<T> other) => this.UnderlyingSet.IsProperSubsetOf(other);
+        public bool IsProperSupersetOf(IEnumerable<T> other) => this.UnderlyingSet.IsProperSupersetOf(other);
+        public bool IsSubsetOf(IEnumerable<T> other) => this.UnderlyingSet.IsSubsetOf(other);
+        public bool IsSupersetOf(IEnumerable<T> other) => this.UnderlyingSet.IsSupersetOf(other);
+        public bool Overlaps(IEnumerable<T> other) => this.UnderlyingSet.Overlaps(other);
+        public bool SetEquals(IEnumerable<T> other) => this.UnderlyingSet.SetEquals(other);
+        public ImmutableSortedSet<T>.Enumerator GetEnumerator() => this.UnderlyingSet.GetEnumerator();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public static bool Equals(SortedSetValue<T> first, SortedSetValue<T> second) => first.SetEquals(second);
+        public static bool operator ==(SortedSetValue<T> first, SortedSetValue<T> second) => Equals(first, second);
+        public static bool operator !=(SortedSetValue<T> first, SortedSetValue<T> second) => !Equals(first, second);
+        public override bool Equals(object obj) => obj is SortedSetValue<T> && Equals(this, (SortedSetValue<T>)obj);
+        public bool Equals(SortedSetValue<T> other) => Equals(this, other);
+        public override int GetHashCode() => GetHashCode(this);
+        public static int GetHashCode(SortedSetValue<T> value)
+        {
+            ImmutableSortedSet<T> underlyingSet = value.UnderlyingSet;
+
+            int hashCode = underlyingSet.Count;
+
+            // We can actually do better than returning right now unconditionally, because
+            // many IComparer<T> implementations also happen to implement IEqualityComparer<T>.
+            IEqualityComparer<T> equalityComparer = underlyingSet.KeyComparer as IEqualityComparer<T>;
+            if (equalityComparer != null)
+            {
+                foreach (T element in underlyingSet)
+                {
+                    hashCode ^= equalityComparer.GetHashCode(element);
+                }
+            }
+
+            return hashCode;
+        }
+    }
+}
