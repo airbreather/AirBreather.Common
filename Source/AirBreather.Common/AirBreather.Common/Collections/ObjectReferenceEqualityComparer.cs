fe4a0268075b8ae3b30c8c1f5ace941b7c4a5425
Joe Amenta <airbreather@linux.com>
2016-01-05 08:34:31 -0500

    Adding object reference equality comparer. Used in cases where object reference equality is desired for equality testing over reference types whose default equality comparer is not appropriate. Expected to be most useful in coded tests, but it may be useful sometimes as a "least evil" fix for a bug.

---
+++ Source/AirBreather.Common/AirBreather.Common/Collections/ObjectReferenceEqualityComparer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AirBreather.Common.Collections
+{
+    public sealed class ObjectReferenceEqualityComparer<T> : EqualityComparer<T> where T : class
+    {
+        public override bool Equals(T x, T y) => ReferenceEquals(x, y);
+        public override int GetHashCode(T obj) => RuntimeHelpers.GetHashCode(obj);
+    }
+}
