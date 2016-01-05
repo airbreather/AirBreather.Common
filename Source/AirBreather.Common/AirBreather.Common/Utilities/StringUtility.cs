d11eeb94dd78a66cd2768c6659e8e3090ec76e18
Joe Amenta <airbreather@linux.com>
2016-01-05 09:05:38 -0500

    Adding more byte array <--> hex string methods. Now there's a hex string --> byte array version that's safer to use. And now there's a byte array --> hex string method.

--- Source/AirBreather.Common/AirBreather.Common/Utilities/StringUtility.cs
+++ Source/AirBreather.Common/AirBreather.Common/Utilities/StringUtility.cs
@@ -1,7 +1,29 @@
-﻿namespace AirBreather.Common.Utilities
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AirBreather.Common.Utilities
 {
-    public static class StringUtility
+    public static unsafe class StringUtility
     {
+        private static readonly Lazy<Regex> hexStringRegex = new Lazy<Regex>(() => new Regex("^([A-Fa-f0-9]{2})*$",
+                                                                                             RegexOptions.CultureInvariant |
+                                                                                             RegexOptions.Compiled |
+                                                                                             RegexOptions.ExplicitCapture),
+                                                                             LazyThreadSafetyMode.PublicationOnly);
+
+        public static byte[] HexStringToByteArrayChecked(this string s)
+        {
+            s.ValidateNotNull(nameof(s));
+            if (!hexStringRegex.Value.IsMatch(s))
+            {
+                throw new ArgumentException("Must provide a hex string.", nameof(s));
+            }
+
+            return s.HexStringToByteArrayUnchecked();
+        }
+
         // http://stackoverflow.com/a/17923942/1083771
         public static byte[] HexStringToByteArrayUnchecked(this string s)
         {
@@ -18,6 +40,42 @@
             }
 
             return bytes;
+        }
+
+        // http://stackoverflow.com/a/24343727/1083771
+        private static readonly uint[] byteTo32BitLookupArray = CreateLookup32Unsafe();
+        private static readonly uint* byteTo32BitLookupPtr = (uint*)GCHandle.Alloc(byteTo32BitLookupArray, GCHandleType.Pinned).AddrOfPinnedObject();
+
+        private static uint[] CreateLookup32Unsafe()
+        {
+            uint[] result = new uint[256];
+            for (int i = 0; i < result.Length; i++)
+            {
+                string s = i.ToString("X2");
+                result[i] = BitConverter.IsLittleEndian
+                    ? s[0] + ((uint)s[1] << 16)
+                    : s[1] + ((uint)s[0] << 16);
+            }
+
+            return result;
+        }
+
+        public static unsafe string ByteArrayToHexString(this byte[] bytes)
+        {
+            uint* lookupP = byteTo32BitLookupPtr;
+            string result = new string((char)0, bytes.Length * 2);
+
+            fixed (byte* bytesP = bytes)
+            fixed (char* resultP = result)
+            {
+                uint* resultP2 = (uint*)resultP;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+
+            return result;
         }
     }
 }
