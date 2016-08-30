a8dba6979fc25525c25dd16a905a0ef91d8dda5c
Joe Amenta <airbreather@linux.com>
2016-08-29 23:31:51 -0400

    Adding a string pool type. Similar to the intern pool, but with user-managed lifetime per instance.

---
+++ Source/AirBreather.Common/AirBreather.Common/Collections/StringPool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace AirBreather.Common.Collections
+{
+    public sealed class StringPool
+    {
+        private readonly Dictionary<string, string> pool = new Dictionary<string, string>();
+
+        public string Pool(string val)
+        {
+            string result;
+            lock (this.pool)
+            {
+                if (!this.pool.TryGetValue(val, out result))
+                {
+                    result = this.pool[val] = val;
+                }
+            }
+
+            return result;
+        }
+
+        public string IsPooled(string val)
+        {
+            lock (this.pool)
+            {
+                this.pool.TryGetValue(val, out val);
+            }
+
+            return val;
+        }
+
+        public void Clear()
+        {
+            lock (this.pool)
+            {
+                this.pool.Clear();
+            }
+        }
+    }
+}
