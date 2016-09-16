94cf233a1225ab9ba1fa51212a47441f6281f5ce
Joe Amenta <airbreather@linux.com>
2016-09-16 09:41:59 -0400

    StringPool now checks the intern pool first.

--- Source/AirBreather.Common/AirBreather.Common/Collections/StringPool.cs
+++ Source/AirBreather.Common/AirBreather.Common/Collections/StringPool.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AirBreather.Common.Collections
 {
     public sealed class StringPool
     {
+        // thought: reimplement to cut down on duplicate hash code checks?  we
+        // could wind up with really long strings here...
         private readonly Dictionary<string, string> pool = new Dictionary<string, string>();
 
         public string Pool(string val)
         {
-            string result;
+            // null is already in a "pool" of its own.
+            if (val == null)
+            {
+                return val;
+            }
+
+            // if it's interned, then we don't need to store it separately.
+            string result = String.IsInterned(val);
+            if (result != null)
+            {
+                return result;
+            }
+
             lock (this.pool)
             {
                 if (!this.pool.TryGetValue(val, out result))
@@ -22,8 +37,24 @@ namespace AirBreather.Common.Collections
 
         public string IsPooled(string val)
         {
+            // null is already in a "pool" of its own.
+            if (val == null)
+            {
+                return val;
+            }
+
+            // if it's interned, then we don't need to check our separate pool.
+            string result = String.IsInterned(val);
+            if (result != null)
+            {
+                return result;
+            }
+
             lock (this.pool)
             {
+                // thought: if something became interned after we pooled it all
+                // custom-like, then maybe we should replace our reference with
+                // a reference to the interned string?
                 this.pool.TryGetValue(val, out val);
             }
 
